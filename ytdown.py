92513c5e046a7b6a6589d658067c70f3e024d64a
Oğuz Bakır <b21627007@cs.hacettepe.edu.tr>
2016-10-29 19:45:53 +0300

    Added txt checker

--- ytdown.py
+++ ytdown.py
@@ -1,13 +1,16 @@
 from subprocess import call
 import time
+import os
 file = open("urls.txt","r")
-call (["echo '\033[1;31m This script requires brew \033[0m'"], shell=True)
-time.sleep(1)
-call (["brew install bash"], shell=True)
-call (["brew install libav"], shell=True)
-call (["brew install youtube-dl"], shell=True)
-for line in file.readlines():
-	liste = line
-	my="youtube-dl -x --audio-format mp3 "+liste
-	call ([my], shell=True)
-file.close()
+if os.path.getsize('urls.txt') == 0:
+	print ("Please enter valid url's to urls.txt")
+else:
+	call (["echo '\033[1;31m This script requires brew \033[0m'"], shell=True)
+	time.sleep(1)
+	call (["brew install bash"], shell=True)
+	call (["brew install libav"], shell=True)
+	call (["brew install youtube-dl"], shell=True)
+	for line in file.readlines():
+		my="youtube-dl -x --audio-format mp3 "+liste
+		call ([my], shell=True)
+	file.close()
