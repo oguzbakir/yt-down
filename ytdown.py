734a00cd036fd912f329bc4c27b059af534cf08a
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2018-04-28 22:55:48 +0300

    Added more platforms

--- ytdown.py
+++ ytdown.py
@@ -1,22 +1,30 @@
-from subprocess import call
-import time
 import os
-file = open("urls.txt","r")
+import platform
+import sys
+from subprocess import call
+
+file = open("urls.txt", "r")
 if os.path.getsize('urls.txt') == 0:
-	print ("Please enter valid url's to urls.txt")
-else:
-	call (["echo '\033[1;31m This script requires brew \033[0m'"], shell=True)
-	time.sleep(1)
-	call (["brew install bash"], shell=True)
-	call (["brew install libav"], shell=True)
-	call (["brew install youtube-dl"], shell=True)
-	k = int(input("1-Mp4\n2-Mp3\nSelect One : "))
-	if k == 2:
-		for line in file.readlines():
-			my="youtube-dl -x --no-check-certificate --audio-format mp3 "+line
-			call ([my], shell=True)
-	if k == 1:
-		for line in file.readlines():
-			my="youtube-dl --no-check-certificate "+line
-			call ([my], shell=True)
-	file.close()
+    print ("Please enter valid url's to urls.txt")
+    sys.exit(0)
+
+if platform.system() == "Darwin":
+    call(["echo '\033[1;31m This script requires brew \033[0m'"], shell=True)
+    call(["brew install bash", "brew install libav", "brew install youtube-dl"], shell=True)
+
+if platform.system() == "Linux":
+    call(["sudo apt-get install youtube-dl"], shell=True)
+
+if platform.system() == "Windows":
+    print("Fuck you i don't support windows")
+
+k = int(input("1-Mp4\n2-Mp3\nSelect One : "))
+if k == 2:
+    for line in file.readlines():
+        my = "youtube-dl -x --no-check-certificate --audio-format mp3 " + line
+        call([my], shell=True)
+if k == 1:
+    for line in file.readlines():
+        my = "youtube-dl --no-check-certificate " + line
+        call([my], shell=True)
+file.close()
